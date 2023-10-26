7d1a04ba92bd18121217121c988ccc390742fef6
rahul <posttorahuldixit@gmail.com>
2023-10-26 09:41:57 +0530

    Object Equals | to String 26.10.2023

--- KIET_Core_Java/src/com/rahul/session07/LangDemo.java
+++ KIET_Core_Java/src/com/rahul/session07/LangDemo.java
@@ -1,17 +1,32 @@
 package com.rahul.session07;
 class AB{
+	private int r;
+	public AB() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public AB(int r) {
+		super();
+		this.r = r;
+	}
 	public String toString() {
         return "This is class AB";
     }
+	public boolean equals(AB a2) {
+		return this.r==a2.r;
+		
+	}
 }
 public class LangDemo {
 public static void main(String[] args) {
-	AB a1 = new AB();
+	AB a1 = new AB(4);
+	AB a2 = new AB(4);
 	
-	String s = a1.toString();
-	System.out.println(s);
 	
-	System.out.println(a1.toString());
-	System.out.println(a1);
+	//String s = a1.toString();
+	//System.out.println(s);
+	
+	//System.out.println(a1.toString());
+	System.out.println(a1.equals(a2));
 }
 }
